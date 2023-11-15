360ad2a9ab726261d196f1b82279008b97143e18
Shantanu <146916282+shanu31396@users.noreply.github.com>
2023-11-15 21:16:40 +0530

    updated

--- tut8.cpp
+++
@@ -1,9 +0,0 @@
-//CONSTANTS, MANIPULATORS AND OPERATOR PRECEDENCE
-#include<iostream>
-using namespace std;
-int main(){
-    const int a = 5;
-    cout<<"Value of a is :"<<a;
-    //a = 6; this line will throw an error as the declared variable is constant;
-        return 0;
-}
