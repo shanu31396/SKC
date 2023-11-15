37fe07ef79f94d87fdc3abaee28380a7cacb0587
Shantanu <146916282+shanu31396@users.noreply.github.com>
2023-11-15 19:02:20 +0530

    update

--- tut8.cpp
+++ tut8.cpp
@@ -2,6 +2,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
-    return 0;
+    const int a = 5;
+    cout<<"Value of a is :"<<a;
+    //a = 6; this line will throw an error as the declared variable is constant;
+        return 0;
 }
