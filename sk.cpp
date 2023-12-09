e348206434edf21d9580ca3554a4e5a026b711de
Shantanu <146916282+shanu31396@users.noreply.github.com>
2023-12-09 15:08:59 +0530

    sk

--- sk.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    cout<<setw(3)<<"hi";
-    return 0;
-}
--- sk_rev.c.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int i;
-    printf("enter a no. : ");
-    scanf("%d",&i);
-    (i%2==0)?printf("even"):printf("odd");
-    return 0;
-}
