92a0a89e5fb430ebb112e880a7184db22be3c915
Shantanu <146916282+shanu31396@users.noreply.github.com>
2023-12-25 17:08:19 +0530

    st

---
+++ tut20.cpp
@@ -0,0 +1,54 @@
+// Introduction to Object Oriented Programming in C++
+
+#include<iostream>
+using namespace std;
+int main(){
+    /*  
+    
+    What is OOPs?
+    It is an approach to make our programs better by eliminating the pitfall of conventional  programming methods, like to make bug free programs
+   
+   
+    WHY OOPS?
+    C++ language was designed with the main intention of adding object-oriented features to C language.
+    As the size of program increases, readability, maintainability and bug-free nature of program decreases.
+    This was the major problem with the languages like C which relied upon functions or procedures (hence, named procedural oriented programming language)
+    As a result, the possibility of  not addressing the problem in an effective manner was high.
+    Also, as data was almost neglected, data security was easily compromised.
+    Using classes solves this problem by modelling program as a real world scenario.
+
+   
+    // PROCEDURAL ORIENTED PROGRAMMING LANGUAGE
+    Main focus is on function not on flow of data
+    Functions can either use local or global data
+    Data moves openly from function to function
+
+    
+    // OBJECT ORIENTED PROGRAMMING LANGUAGE
+    Works on the concept of classes and objects
+    Treats data as a critical element
+    **************************************Decomposes the problem in OBJECTS and builds data and function around OBJECTS **************************************
+
+    Classes are just like templates and they don't consume memory but when we make objects from class(i.e. template) it takes memory 
+    according to variables defined in that class
+
+
+    // BASIC TERMINOLOGY USED IN OOPs
+    Classes:-   basic template for creating objects 
+    Object:-    basic run time entities
+    (Data abstraction) & (Encapsulation):-  (Wrapping data and functions) into (single unit)
+    Inheritance:-   Properties of one class can be inherited into others
+    Polymorphism:-  ability to take more than one form
+    Dynamic Binding:-   code which will execute is not known until the program runs
+    Message Passing:-   Object.messge(Information) call format
+
+
+    // BENEFITS OF OBJECT ORIENTED PROGRAMMING
+    Better code reusability using objects and inheritance
+    Principle of data hiding helps build secure systems
+    Multiple objects can co-exist without any interference
+    Software complexity can be managed easily
+ 
+   */
+    return 0;
+}
