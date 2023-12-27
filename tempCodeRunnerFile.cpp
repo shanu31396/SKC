1e98af335f5406509c80cd7c1cd2d987f119eb6d
Shantanu <146916282+shanu31396@users.noreply.github.com>
2023-12-27 13:31:47 +0530

    sk

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-    for (int i = 0; i < 4; i++)
-    {
-        cout<<i<<endl;
-        if(i==2){
-            break; // here, execution stops completely even without looking for new condition
-        }
