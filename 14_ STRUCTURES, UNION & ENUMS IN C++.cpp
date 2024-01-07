cb9cecfd19889002b1e25ce222e939f4dcba1f07
Shantanu <146916282+shanu31396@users.noreply.github.com>
2024-01-08 00:19:35 +0530

    q

--- 14_ STRUCTURES, UNION & ENUMS IN C++.cpp
+++ 14_ STRUCTURES, UNION & ENUMS IN C++.cpp
@@ -6,7 +6,6 @@ using namespace std;
 //Struct, union and enum are user-defined datatypes 
 typedef union employ
     {
-        /* data */
         int eId;    // 4 bytes will be used
         char favChar;   // 1 byte will be used
         float sal;  // 4 bytes will be used
@@ -15,32 +14,26 @@ typedef union employ
         // if we had created a structure, 9 bytes would allocate here
 
         /*
-        we are creating struct employ as a datatype in which we defined necessary built-in datatypes;   [line 7 to 12]
-        now, we can use employees names as variables with newly defined datatype [line 54 56 58]
+        we are creating struct employ as a datatype in which we define necessary built-in datatypes;   [line 7 to 12]
+        now, we can use employees names as variables with newly defined datatype 
         now, we can treat it as normal variable just put variable name and enter value in below format
-        [variable name.required variable name defined in struct]   [line 59] 
-        then we can use it with cout [line 71]
+        [variable name.required variable name defined in struct]  
+        then we can use it with cout
         */
 
 
-
-
-    // typedef union money
+ // typedef union money
     // {
-    //     /* data */
     //     int rice; //4 byte
     //     char car; //1 byte
     //     float pounds; //4 byte
 
-    // }um;
+// }um;
   
-        // here, in defining union, C++ will allocate a combined memory for all variable
+        // here, while defining union, C++ will allocate a combined memory for all variable
         // so,min. 1 and max. 4 bytes of space will be taken here in memory as per need
-        // koi 1 to koi 4 lega, agr 1 wala mangega to 1 hi denge agr koi kuchh ni bola to 4 le lenge,fir 1 ka demand hua 
-        // to 1 dekar memory save kr lenge aur 4 manga to apne pass 4 bytes to hai hi
-
-        // here, in this user-defined datatype, we can use one datatype only once
-        // so, here we'll use very important feature of C++ which is memory management
+        // since, we can use only one datatype so maximum memory usage will be 4 bytes 
+        // in this way we are using very important feature of C++ which is memory management
         // union provides better meomry management   
         // C++ will share memory among these here
 
@@ -63,8 +56,10 @@ int main(){
     // um sl;
     // sl.car = 'a';
     // cout<<sl.car;
+
     // Structure is used to make a collection of objects that are not of similar type
     // It is just inverse of array because in array we make collection of items of similar type 
+
     // um m1;
     // m1.rice = 34;
     // cout<<m1.rice<<endl;
@@ -78,7 +73,7 @@ int main(){
     sk.eId = 1;
     cout<<"The value is "<<sk.eId<<endl;
     sk.favChar = 'c';
-      cout<<"The value is "<<sk.favChar<<endl;
+    cout<<"The value is "<<sk.favChar<<endl;
     sk.sal = 9;
     
     sr.eId = 2;
@@ -91,30 +86,27 @@ int main(){
     st.sal = 91;
       
     /*
-    THE DIFFERENCE BETWEEN STRUCT AND UNION IS THAT STRUCT ONLY UPDATES THE VARIABLE WHOSE VALUE IS UPDATED USING DOT (i.e. sk.abc=5;    sk.abc=6) AND PRINT REST AS IT WAS
+    THE DIFFERENCE BETWEEN STRUCT AND UNION IS THAT STRUCT ONLY UPDATES THE VARIABLE WHOSE VALUE IS UPDATED USING DOT (i.e. sk.abc=5;    sk.abc=6) AND PRINT REST AS IT IS
     WHILE IN CASE OF UNION IT ONLY CONSIDERES THE LAST DATATYPE AS MAIN DATATYPE AND LEFT BEHIND THE REST DATATYPES AS GARBAGE VALUE IRRESPECTIVE OF WHAT KIND OF DATATYPE IS THAT
     */
-
-
-  
-    cout<<"The value is "<<sk.sal<<endl<<endl;
     
+    cout<<"The value is "<<sk.sal<<endl<<endl;
     cout<<"The value is "<<sr.eId<<endl;
-    cout<<"The value of favchar is      "<<sr.favChar<<endl;
-    cout<<"The value of sal is   "<<sr.sal<<endl<<endl;   
+    cout<<"The value of favchar is "<<sr.favChar<<endl;
+    cout<<"The value of sal is "<<sr.sal<<endl<<endl;   
     
     cout<<"The value is "<<st.eId<<endl;
     cout<<"The value is "<<st.favChar<<endl;
     cout<<"The value is "<<st.sal<<endl<<endl;
     
-    // enum meal{breakfast, lunch, dinner};     // user-defined datatype declared
-    // meal m1 = lunch;                         // new_datatype variable_name = already defined input
-    // cout<<m1<<endl;                          // printing the variable's value which shows position of value entered in that variable
-    // cout<<(m1==2)<<endl;                     // // printing variable's value by checking the given condition
+    enum meal{breakfast, lunch, dinner};     // user-defined datatype declared
+    meal m1 = lunch;                         // new_datatype variable_name = already defined input
+    cout<<m1<<endl;                          // printing the variable's value which shows position of value entered in that variable
+    cout<<(m1==1)<<endl;                     // // printing variable's value by checking the given condition
 
-    // enum deal{d9,d8,d7};
-    // deal D9=d9;
-    // cout<<D9<<endl;
+    enum deal{d9,d8,d7};
+    deal D9=d9;
+    cout<<D9<<endl;
 
     
 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+enum meal{breakfast, lunch, dinner};     // user-defined datatype declared
+    meal m1 = lunch;                         // new_datatype variable_name = already defined input
+    cout<<m1<<endl;                          // printing the variable's value which shows position of value entered in that variable
+    cout<<(m1==2)<<endl;                     // // printing variable's value by checking the given condition
+
+    enum deal{d9,d8,d7};
+    deal D9=d9;
+    cout<<D9<<endl;
