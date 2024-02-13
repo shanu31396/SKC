1157e76855c5d592bc328ab76925c7e9f24c38a4
shanu31396 <146916282+shanu31396@users.noreply.github.com>
2024-02-13 22:18:46 +0530

    Update matrix_multi.cpp

--- matrix_multi.cpp
+++ matrix_multi.cpp
@@ -11,7 +11,7 @@ main(){int i,j,k,r1,r2,c1,c2,s=0;
     for(int i=0;i<r1;i++){
         for(int j=0;j<c2;j++){
             for(int k=0;k<r2;k++){
-                s+=a[i][k]+b[k][j];}
+                s+=a[i][k]*b[k][j];}
         c[i][j]=s;s=0;}}
     for(int i=0;i<r1;i++){
         for(int j=0;j<c2;j++){
