03480c56ce7c50952d6e85aa650e1b2040295dac
Shantanu Thakur <shanu31396@gmail.com>
2025-01-09 14:11:55 +0530

    sk

---
+++ array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[] = {25, 35, 45, 55};
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout<<"The value of "<<i+1<<"st mark is "<<marks[i]<<endl;
+    // }
+
+    
+    // int i = 0;
+    // while (i < 4)
+    // {
+    //     cout << "The value of " << i + 1 << "st mark is " << marks[i] << endl;
+    //     i++;
+    // }
+
+
+    // int i=0;
+    // do
+    // {
+    //        cout << "The value of " << i + 1 << "st mark is " << marks[i] << endl;
+    //        i++;
+    // } while (i<4);
+
+
+    // int * p = marks;
+    // for (int i = 0; i < 4; i++,p++)
+    // {
+    //     cout<<"The value of "<<i+1<<"st mark is "<<*p<<endl;
+    // }
+    
+    
+
+    return 0;
+}
